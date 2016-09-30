fe27761a487b6d5a880f0e7be8f072b9201b1ca7
pipmix <onloadx@yahoo.com>
2016-09-30 01:39:30 -0700

    fix

--- BasicLightingVS.hlsl
+++ BasicLightingVS.hlsl
@@ -1,3 +1,5 @@
+//#pragma pack_matrix( row_major )
+
 cbuffer cbPerFrame : register(b0) {
 	matrix cameraScreenMatrix;
 	matrix cameraMatrix;
@@ -29,26 +31,42 @@ VS_OUT main(float4 position : POSITION, float4 normal : NORMAL, float2 texcoord 
 
 	VS_OUT vs_out;
 
-	float4x4 finalMatrix = mul(cameraScreenMatrix, worldMatrix);
+	//float4 posWorld = mul(float4(position, 1), worldMatrix);
+
+	//vs_out.position = mul(mul(posWorld, cameraMatrix), screenMatrix);
+
+	matrix final; 
+
+
+	//final = mul(screenMatrix, cameraMatrix);
+	//final = mul(mul(screenMatrix, cameraMatrix), worldMatrix);
+
+	
+	//vs_out.position = mul(mul(mul(position, worldMatrix), cameraMatrix), screenMatrix);
+
+	//float4x4 finalMatrix = mul(cameraScreenMatrix, worldMatrix);
+
+
+
+
 
 	float4 col = float4(0.2, 0.2, 0.2, 1.0);
 	float lgtVec = float4(1.0f, 1.0f, 1.0f, 0.0f);
 	float lgtCol = float4(0.5, 0.5, 0.5, 1.0);
 
 
-	float4x4 lightMatix = mul(cameraMatrix, worldMatrix);
-	float4 norm = normalize(mul(worldMatrix, normal));
 
-	float diffusebrightness = saturate(dot(norm, lgtVec));    // calculate the diffuse brightness
+	
+
+	float diffusebrightness = saturate(dot(lgtVec, normalize(mul(worldMatrix, normal))));    // calculate the diffuse brightness
 
 
 
-	vs_out.position = mul(finalMatrix, position);
-
+	//vs_out.position = mul(finalMatrix, position);
+	vs_out.position = mul(screenMatrix, mul(cameraMatrix, mul(worldMatrix, position)));
 	vs_out.uv = texcoord;
 
-	vs_out.color = col;
-	vs_out.color += lgtCol * diffusebrightness;    // find the diffuse color and add
+	vs_out.color = col + (lgtCol * diffusebrightness);    // find the diffuse color and add
 
 
 	return vs_out;
